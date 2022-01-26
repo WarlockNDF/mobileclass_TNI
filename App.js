d98b5403878d6b2aedd21d91a74cfb0ea39e1b9f
WarlockNDF <70856081+WarlockNDF@users.noreply.github.com>
2022-01-26 12:03:18 +0700

    Drawer

--- App.js
+++ App.js
@@ -1,30 +1,76 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View,TouchableOpacity,Image } from 'react-native';
 import HomeStack from './navigation/stack/HomeStack';
 import DetailStack from './navigation/stack/DetailStack';
 import FirstPage from './component/pages/firstpage';
 import SecondPage from './component/pages/secondpage';
 import ThirdPage from './component/pages/thirdpage';
+import {createDrawerNavigator} from "@react-navigation/drawer"
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function firstScreenStack({navigation}){
+  return(
+       <Stack.Navigator initialRouteName="FirstPage"
+          screenOptions = {{
+            headerStyle:{backgroundColor: "#24E3AD"},
+            headerTintColor:"white",
+            headerTitleStyle:{fontWeight:"bold"},
+            headerLeft: () => <NavigationDrawerStructure navigationProps={navigation} />
+          }}>
+          <Stack.Screen name='FirstPage' component={FirstPage}/>
+        </Stack.Navigator>
+  )
+}
+
+function seccondScreenStack({navigation}){
+  return (
+    <Stack.Navigator initialRouteName="FirstPage"
+    screenOptions = {{
+      headerStyle:{backgroundColor: "#24E3AD"},
+      headerTintColor:"white",
+      headerTitleStyle:{fontWeight:"bold"},
+      headerLeft: () => <NavigationDrawerStructure navigationProps={navigation} />
+    }}>
+    <Stack.Screen name='SecondPage' component={SecondPage}/>
+    <Stack.Screen name='ThirdPage' component={ThirdPage}/>
+  </Stack.Navigator>
+  )
+}
+
+const NavigationDrawerStructure = (props) => {
+
+  const toggleDrawer = () => {
+    props.navigationProps.toggleDrawer();
+  }
+
+  return(
+    <View style={{flexDirection:"row"}}>
+      <TouchableOpacity onPress={() => toggleDrawer()}>
+      <MaterialCommunityIcons style={{marginLeft: 20}} name="account-details" size={26} color="white" />
+        {/* <Image
+          source={"./assets/drawerWhite.png"}
+          style={{width:25, height:25, marginLeft: 5}}
+        /> */}
+      </TouchableOpacity>
+    </View>
+  )
+
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <Text>This is Navigation Class NAJA</Text> */}
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="HomeStack"
-          screenOptions = {{
-            headerStyle:{backgroundColor: "#24E3AD"},
-            headerTintColor:"white",
-            headerTitleStyle:{fontWeight:"bold"}
-          }}>
-          <Stack.Screen name='FirstPage' component={FirstPage}/>
-          <Stack.Screen name='SecondPage' component={SecondPage}/>
-          <Stack.Screen name='ThirdPage' component={ThirdPage}/>
-        </Stack.Navigator>
+        <Drawer.Navigator>
+          <Drawer.Screen name='FirstDrawerPage' component={firstScreenStack}/> 
+          <Drawer.Screen name='SecondDrawerPage' component={seccondScreenStack}/>
+        </Drawer.Navigator>
       </NavigationContainer>
       <StatusBar style="auto" />
     </View>
